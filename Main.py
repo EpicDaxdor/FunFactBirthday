3777a4f5f047d8a4e58ddcd18efa033b36101308
John Thomson <johndavthomson@proton.me>
2024-05-30 18:02:32 +0100

    My First Personal Project

---
+++ Main.py
@@ -0,0 +1,46 @@
+import time
+
+time.sleep
+
+print("Hello, Stranger!")
+time.sleep(3)
+print ("I don't know you, So lets change that, Shall we ?")
+time.sleep(3)
+print("I have got some questions for you, If you don't mind ?")
+time.sleep(3)
+name = input("Whats is your first name? ")
+time.sleep(2)
+print("Ahh.. Hey there",name, "..Nice to meet you")
+time.sleep(2)
+print ("My name is Alice, I am a chatbot to the stars ... well I like to think so. Anyway")
+time.sleep(3)
+print("So",name, "..... Hmm, What a Great Name, It's a nice family name" )
+print("Sorry to ask you this but....")
+time.sleep(8)
+age = input("How old are you? ")
+
+# Calculation of how old you are in days
+days = int(age) * 365.25
+
+print("Really .. That doesn't sound right")
+time.sleep(4)
+print("You don't look a day over 21, But I am sure you get told that all the time... ")
+time.sleep(3)
+print("Did you know that you have been on this planet for" ,days, "days ? ")
+time.sleep(2)
+print("Doesn't that sound like a lot of days?")
+time.sleep(4)
+print("Well I have a little fun fact for you")
+time.sleep(2)
+weight = input("How much do you weigh in pounds? ")
+time.sleep(2)
+
+# How much you would weigh on the moon
+moon = int(weight)/9.81 * 1.622
+
+print("On the moon you would only weigh ", moon,"lbs")
+time.sleep(4)
+print("One violent sneeze or One light silent but violent fart and you'll fly off into space ... byeeeeee!!")
+time.sleep(2)
+#height = input("How tall are you in centimeters?")
+time.sleep(2)
